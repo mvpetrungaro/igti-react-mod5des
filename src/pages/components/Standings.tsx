49d1cd254a48df715fa4f3fb127e032be4995eb6
marcos.petrungaro <marcos.petrungaro@ibge.gov.br>
2022-09-22 15:30:15 -0300

    Showing teams logo

--- src/pages/components/Standings.tsx
+++ src/pages/components/Standings.tsx
@@ -1,15 +1,24 @@
-import { TeamRecord } from '../../models/TeamRecord'
-import styles from './Standings.module.css'
+import { TeamRecord } from "../../models/TeamRecord";
+import styles from "./Standings.module.css";
 
-type StandingsParams = { teamsRecords: TeamRecord[] }
+type StandingsParams = { teamsRecords: TeamRecord[] };
+
+function getImgSrcFromTeam(team: string): string {
+  return `/img/${team
+    .trim()
+    .toLowerCase()
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "")
+    .replace(/\s+/g, "_")}.png`;
+}
 
 export default function Standings({ teamsRecords }: StandingsParams) {
   return (
     <div>
-      <table className="border">
+      <table className="standings-table border">
         <thead>
-          <tr className={`border ${styles['space-between-cells']}`}>
-            <th colSpan={2}>CLASSIFICAÇÃO</th>
+          <tr className={`border ${styles["space-between-cells"]}`}>
+            <th colSpan={3}>CLASSIFICAÇÃO</th>
             <th>P</th>
             <th>J</th>
             <th>V</th>
@@ -22,8 +31,15 @@ export default function Standings({ teamsRecords }: StandingsParams) {
         </thead>
         <tbody>
           {teamsRecords.map((teamRecord, i) => (
-            <tr key={i} className={`border ${styles['space-between-cells']}`}>
+            <tr key={i} className={`border ${styles["space-between-cells"]}`}>
               <td>{i + 1}</td>
+              <td>
+                <img
+                  src={getImgSrcFromTeam(teamRecord.team)}
+                  alt={teamRecord.team}
+                  style={{ width: "1.5em" }}
+                />
+              </td>
               <td>{teamRecord.team}</td>
               <td>{teamRecord.record.total_pontos}</td>
               <td>{teamRecord.record.total_jogos}</td>
@@ -41,5 +57,5 @@ export default function Standings({ teamsRecords }: StandingsParams) {
         </tbody>
       </table>
     </div>
-  )
+  );
 }
